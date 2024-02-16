82c71f8a7a0dc69d326f0ab238f37a1e7b185b74
Jordy Sanipatin <105766977+JordySa@users.noreply.github.com>
2024-02-16 02:12:38 -0500

    Update header.php

--- application/views/header.php
+++ application/views/header.php
@@ -10,23 +10,23 @@
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url(); ?>/assets/images/favicon.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script src="<?php echo base_url(); ?>/assets/vendor/jquery/jquery.min.js"></script>
-    <link href="<?php echo base_url(); ?>/assets/vendor/datatables/css/jquery.dataTables.min.css" rel="stylesheet">
-    <script src="<?php echo base_url(); ?>/assets/js/plugins-init/datatables.init.js"></script>
-    <script src="<?php echo base_url(); ?>/assets/vendor/datatables/js/jquery.dataTables.min.js"></script>
-    <script src="<?php echo base_url(); ?>/assets/js/plugins-init/datatables.init.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendor/jquery/jquery.min.js"></script>
+    <link href="<?php echo base_url(); ?>assets/vendor/datatables/css/jquery.dataTables.min.css" rel="stylesheet">
+    <script src="<?php echo base_url(); ?>assets/js/plugins-init/datatables.init.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendor/datatables/js/jquery.dataTables.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/plugins-init/datatables.init.js"></script>
         <!-- Jquery Validation -->
-        <script src="<?php echo base_url(); ?>/assets/vendor/jquery-validation/jquery.validate.min.js"></script>
+        <script src="<?php echo base_url(); ?>assets/vendor/jquery-validation/jquery.validate.min.js"></script>
     <!-- Form validate init -->
-    <script src="<?php echo base_url(); ?>/assets/js/plugins-init/jquery.validate-init.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/plugins-init/jquery.validate-init.js"></script>
 
-    <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/vendor/owl-carousel/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/vendor/owl-carousel/css/owl.theme.default.min.css">
-    <link href="<?php echo base_url(); ?>/assets/vendor/jqvmap/css/jqvmap.min.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>/assets/css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendor/owl-carousel/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendor/owl-carousel/css/owl.theme.default.min.css">
+    <link href="<?php echo base_url(); ?>assets/vendor/jqvmap/css/jqvmap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet">
 
     <!-- Custom Stylesheet -->
-    <link href="<?php echo base_url(); ?>/assets/css/style.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet">
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/izitoast/1.4.0/js/iziToast.min.js" integrity="sha512-Zq9o+E00xhhR/7vJ49mxFNJ0KQw1E1TMWkPTxrWcnpfEFDEXgUiwJHIKit93EW/XxE31HSI5GEOW06G6BF1AtA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/izitoast/1.4.0/css/iziToast.css" integrity="sha512-DIW4FkYTOxjCqRt7oS9BFO+nVOwDL4bzukDyDtMO7crjUZhwpyrWBFroq+IqRe6VnJkTpRAS6nhDvf0w+wHmxg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -62,9 +62,9 @@
         ***********************************-->
         <div class="nav-header">
             <a href="<?php echo base_url(); ?>/" class="brand-logo">
-                <img class="logo-abbr" src="<?php echo base_url(); ?>/assets/images/logo.png" alt="">
-                <img class="logo-compact" src="<?php echo base_url(); ?>/assets/images/logo-text.png" alt="">
-                <img class="brand-title" src="<?php echo base_url(); ?>/assets/images/logo-text.png" alt="">
+                <img class="logo-abbr" src="<?php echo base_url(); ?>assets/images/logo.png" alt="">
+                <img class="logo-compact" src="<?php echo base_url(); ?>assets/images/logo-text.png" alt="">
+                <img class="brand-title" src="<?php echo base_url(); ?>assets/images/logo-text.png" alt="">
             </a>
 
             <div class="nav-control">
