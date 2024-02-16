a5ed86fcab9c541e5a32ace7988d3735a90d72ca
Jordy Sanipatin <105766977+JordySa@users.noreply.github.com>
2024-02-16 01:50:46 -0500

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/junta_agua_utc/';
+$config['base_url'] = 'https://aquaguardutc.azurewebsites.net/';
 
 /*
 |--------------------------------------------------------------------------
